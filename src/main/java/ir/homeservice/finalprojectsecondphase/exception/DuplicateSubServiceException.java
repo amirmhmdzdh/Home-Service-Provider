9a01cd76ff44ee620b54b0c67851d1e829998d4c
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-06-18 16:57:48 +0430

    DuplicateSubServiceException

---
+++ src/main/java/ir/homeservice/finalprojectsecondphase/exception/DuplicateSubServiceException.java
@@ -0,0 +1,7 @@
+package ir.homeservice.finalprojectsecondphase.exception;
+
+public class DuplicateSubServiceException extends RuntimeException{
+    public DuplicateSubServiceException(String message) {
+        super(message);
+    }
+}
