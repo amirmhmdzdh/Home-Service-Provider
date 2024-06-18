87d17dd4b04d1e8028cfa7c97e0c651c9f33b924
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-06-18 07:22:21 +0430

    Create service AddressService.java

---
+++ src/main/java/ir/homeservice/finalprojectsecondphase/service/AddressService.java
@@ -0,0 +1,18 @@
+package ir.homeservice.finalprojectsecondphase.service;
+
+import ir.homeservice.finalprojectsecondphase.model.address.Address;
+import ir.homeservice.finalprojectsecondphase.repository.AddressRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AddressService {
+    private final AddressRepository addressRepository;
+
+    public void createAddress(Address address) {
+        addressRepository.save(address);
+    }
+}
