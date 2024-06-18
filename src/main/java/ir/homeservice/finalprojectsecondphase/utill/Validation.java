273167e6884740f609d539bc2db92606f76311b0
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-06-18 06:43:15 +0430

    add Validation.java

---
+++ src/main/java/ir/homeservice/finalprojectsecondphase/utill/Validation.java
@@ -0,0 +1,108 @@
+package ir.homeservice.finalprojectsecondphase.utill;
+
+
+import ir.homeservice.finalprojectsecondphase.exception.*;
+import ir.homeservice.finalprojectsecondphase.model.order.Orders;
+import ir.homeservice.finalprojectsecondphase.model.service.SubService;
+import ir.homeservice.finalprojectsecondphase.model.user.Customer;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.regex.Pattern;
+
+@Component
+public class Validation {
+
+    public boolean checkBlank(String str) {
+        if (str.isBlank()) throw new TextBlankException("the string input is blank!");
+        return true;
+    }
+
+    public boolean checkText(String text) {
+        String regex = "^[a-zA-Z ]{3,}$";
+        if (!Pattern.matches(regex, text))
+            throw new AlphabetException("the wording of the text is not incorrect!");
+        return true;
+    }
+
+    public boolean checkPositiveNumber(Long longDigit) {
+        if (longDigit <= 0)
+            throw new PositiveNumberException("the number is not Valid");
+        return true;
+    }
+
+    public boolean checkEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        if (!Pattern.matches(emailRegex, email))
+            throw new EmailFormatException("the format of the email is incorrect!");
+        return true;
+    }
+
+    public boolean checkPassword(String password) {
+        String passwordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        if (!Pattern.matches(passwordRegex, password))
+            throw new PasswordFormatException("the format of the password is incorrect!");
+        return true;
+    }
+
+    public byte[] checkImage(String imagePath) {
+        if (imagePath == null) {
+            throw new ImageFormatException("The image is empty!");
+        }
+
+        if (imagePath.matches(".*\\.(jpg|jpeg)$")) {
+            throw new ImageFormatException("Invalid file format. Only JPG and JPEG formats are supported.");
+        }
+
+        byte[] imageData = null;
+        try {
+            imageData = Files.readAllBytes(Paths.get(imagePath));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (imageData != null && imageData.length > 300 * 1024) {
+            throw new ImageFormatException("The size of the image exceeds the limit!");
+        }
+        return imageData;
+    }
+
+    public boolean checkOfferBelongToTheOrder(Long offerId, Customer customer) {
+        boolean[] exist = new boolean[1];
+        customer.getOrdersList().forEach(order -> exist[0] = order.getOfferList().stream().filter(offer ->
+                offer.getId().equals(offerId)).findFirst().isEmpty());
+        if (exist[0])
+            throw new OfferNotExistException("this offer not belong to your orders");
+        return true;
+    }
+
+    public boolean checkOwnerOfTheOrder(Long orderId, Customer customer) {
+        if (customer.getOrdersList().stream().filter(o -> o.getId().equals(orderId)).findFirst().isEmpty())
+            throw new OrderIsNotExistException("you are not the owner of this order");
+        return true;
+    }
+    public void validatePrice(SubService subService, Orders order) {
+        if (subService.getBasePrice() > order.getProposedPrice()) {
+            throw new PriceException("The suggested price can be lower than the base price.");
+        }
+    }
+    public void validateTime(Orders orders) {
+        if (orders.getExecutionTime().isBefore(LocalDateTime.now())) {
+            throw new TimeException("Execution time before current time");
+        }
+
+        if (orders.getEndTime().isBefore(orders.getExecutionTime())) {
+            throw new TimeException("End time before execution time");
+        }
+    }
+
+
+
+
+
+
+
+}
