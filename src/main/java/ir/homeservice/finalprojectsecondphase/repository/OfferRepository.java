4472d2613889da522a051b2811c40c69325e2e0d
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-06-19 10:06:42 +0430

    creat 2 query for find offer custom

--- src/main/java/ir/homeservice/finalprojectsecondphase/repository/OfferRepository.java
+++ src/main/java/ir/homeservice/finalprojectsecondphase/repository/OfferRepository.java
@@ -1,7 +1,17 @@
 package ir.homeservice.finalprojectsecondphase.repository;
 
 import ir.homeservice.finalprojectsecondphase.model.offer.Offer;
+import ir.homeservice.finalprojectsecondphase.model.offer.enums.OfferStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface OfferRepository extends JpaRepository<Offer,Long> {
+import java.util.List;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+    @Query(" from Offer o where o.orders.id = :orderId and o.offerStatus= :offerStatus order by o.proposedPrice asc")
+    List<Offer> findOfferListByProposedPrice(Long orderId, OfferStatus offerStatus);
+
+    @Query(" from Offer o where o.orders.id = :orderId and o.offerStatus= :offerStatus order by o.specialist.star desc")
+    List<Offer> findOfferListBySpecialistScore(Long orderId, OfferStatus offerStatus);
 }
